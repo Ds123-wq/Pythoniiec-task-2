137c1551ce0a16389dcb255b69e460006e13fc47
Ds123-wq <64480480+Ds123-wq@users.noreply.github.com>
2020-10-06 16:44:08 +0530

    task uploades

---
+++ cmd.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import cgi
+
+print("content-type: text/html")
+print()
+
+form = cgi.FieldStorage()
+cmd = form.getvalue("x")
+
+print(cmd)
+
+import subprocess
+x=subprocess.getoutput(cmd)
+print(x)
+
