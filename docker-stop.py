e464e366ae39eb0d59d3152e11084800ba38986d
Ds123-wq <64480480+Ds123-wq@users.noreply.github.com>
2020-10-08 12:06:42 +0530

    docker files

---
+++ docker-stop.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+print("content-type: text/html")
+print()
+
+import subprocess as sp
+import cgi
+form = cgi.FieldStorage()
+
+osname=form.getvalue("x")
+
+cmd = "sudo docker stop {}".format(osname)
+
+output = sp.getstatusoutput(cmd)
+
+status = output[0]
+out = output[1]
+
+
+if status == 0:
+    print("os successfully stopped  named {}  ..".format(osname))
+else:
+    print("some error ... {} ".format(out))
